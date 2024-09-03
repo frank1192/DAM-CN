6b37df7eabe9bcda0f1347893a7050e002e1e206
Frankdvop <franklin.aguirre@correounivalle.edu.co>
2024-09-02 23:34:10 -0500

    databinding funcionando

---
+++ app/src/main/java/com/example/calcularnota/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.calcularnota
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.calcularnota.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+}
